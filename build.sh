c58a1d460cf49493ec30d8344a14bb762f29c1bb
EVGESHAd <evgeniy.d@informmonitoring.ru>
2019-09-21 12:18:46 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 export ANDROID_NDK_ROOT=/opt/android-ndk
 export ANDROID_SDK_ROOT=/opt/android-sdk
 
-git clone https://github.com/kennethreitz/requests
-cp -r requests/requests Lib
-git clone https://github.com/lorien/captcha_solver
-cp -r captcha_solver/captcha_solver Lib/
-git clone https://github.com/urllib3/urllib3
-cp -r urllib3/src/urllib3 Lib
-git clone https://github.com/mysql/mysql-connector-python
-cp -r mysql-connector-python/lib/mysql* Lib
-
 cd Android
 #make clean
 
